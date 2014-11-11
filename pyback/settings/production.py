e34e2fb9fab32bb612337ba30ee183a86d6e0af5
Ararat Poghosyan <ararat.poghosyan@gmail.com>
2014-11-11 19:23:38 +0400

    new  directory structure

--- pyback/settings/production.py
+++ pyback/settings/production.py
@@ -86,7 +86,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../Python/../../../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
 
