7d9648ed3f5bad33fc727ba21886f3ec419cae0e
Ararat Poghosyan <ararat.poghosyan@gmail.com>
2014-12-12 11:59:58 +0400

    added puppet provisioning

---
+++ pyback/apps/community/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Community',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=255)),
+                ('description', models.TextField()),
+                ('type', models.IntegerField()),
+                ('status', models.IntegerField()),
+                ('code', models.CharField(max_length=8)),
+                ('ordering', models.IntegerField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
